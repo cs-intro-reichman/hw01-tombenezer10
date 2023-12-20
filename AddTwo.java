d343ae952b61ef1422831056c8a6a7c63fd42814
Tom Ben Ezer <tom.benezer@runi.ac.il>
2023-12-20 09:40:45 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,8 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println(a + " + " + b + " = " + (a + b));
 	}
 }
--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int cents = Integer.parseInt(args[0]);
+		int quarters = cents / 25;
+		cents = cents % 25;
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
